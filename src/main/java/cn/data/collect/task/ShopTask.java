70f1aa5a161a2bc66b0479c5fab5fa30c5ec519b
zengchao <zengchaocn@yeah.net>
2018-09-07 17:07:47 +0800

    jsoup add

--- src/main/java/cn/data/collect/task/ShopTask.java
+++ src/main/java/cn/data/collect/task/ShopTask.java
@@ -1,10 +1,19 @@
 package cn.data.collect.task;
 
+import cn.data.collect.util.Constants;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShopTask implements Runnable {
     private static Logger logger = LoggerFactory.getLogger(ShopTask.class);
+    private static final int LIMIT = 24;
+    private static final int DEEP = 1;
 
     private Integer taskId;
     private String taskName;
@@ -12,6 +21,7 @@ public class ShopTask implements Runnable {
     @Override
     public void run() {
         logger.info("{}:{} run", taskName, taskId);
+
     }
 
     public Integer getTaskId() {
@@ -28,5 +38,24 @@ public class ShopTask implements Runnable {
 
     public void setTaskName(String taskName) {
         this.taskName = taskName;
+    }
+
+    private String getJsonString() throws IOException {
+        Map<String, String> argMap = new HashMap<>();
+        argMap.put("extras[]", "activities");
+        argMap.put("geohash", "ws0edengxq12");
+        argMap.put("latitude", "23.132347");
+        argMap.put("longitude", "113.322571");
+        argMap.put("limit", "100");
+        argMap.put("offset", "0");
+        argMap.put("terminal", "web");
+        Document document = Jsoup.connect(Constants.E_SCAN_SHOP_URL)
+                .header("Accept", "application/json, text/plain, */*")
+                .header("Accept-Encoding", "gzip, deflate, br")
+                .header("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+                .header("Cache-Control", "no-cache")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36")
+                .ignoreContentType(true).data(argMap).get();
+        return document.text();
     }
 }
---
+++ src/main/java/cn/data/collect/util/Constants.java
@@ -0,0 +1,5 @@
+package cn.data.collect.util;
+
+public class Constants {
+    public static final String E_SCAN_SHOP_URL = "https://www.ele.me/restapi/shopping/restaurants";
+}
